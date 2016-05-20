5a7e8d5f8fcc89259cd752c42092a652239771f4
Mohammad Alaggan <mohammad.nabil.h@gmail.com>
2016-05-21 00:13:07 +0200

    Global variable marked

--- CFG.lisp
+++ CFG.lisp
@@ -1,5 +1,5 @@
 (defparameter *basic-blocks* (make-array 1 :fill-pointer 0 :adjustable t))
-(defparameter statements     (make-array 1 :fill-pointer 0 :adjustable t))
+(defparameter *statements*     (make-array 1 :fill-pointer 0 :adjustable t))
 
 (defclass statement-block ()
 	((statement								:accessor statement								:initarg :statement )
@@ -10,7 +10,7 @@
 	 (predecessor-statements	:accessor predecessor-statements	:initarg :predecessor-statements)))
 
 (defclass basic-block ()
-	((leader							:accessor leader							:initarg :leader)
+	((frst-stmt						:accessor frst-stmt						:initarg :frst-stmt)
 	 (last-stmt						:accessor last-stmt						:initarg :last-stmt)
 	 (next-block					:accessor next-block)
 	 (predecessor-blocks	:accessor predecessor-blocks	:initarg :predecessor-blocks)))
@@ -21,27 +21,26 @@
 											 'if)
 									 (push-statement (list (nth 0 stmt)
 																				 (nth 1 stmt))
-																	 (predecessors 1 statements))
+																	 (predecessors 1 *statements*))
 									 (push-statement (nth 2 stmt)
-																	 (predecessors 1 statements))
+																	 (predecessors 1 *statements*))
 									 (push-statement (nth 3 stmt)
-																	 (predecessors 2 statements)))
+																	 (predecessors 2 *statements*)))
 									((or (eq (car stmt)
 													 'when)
 											 (eq (car stmt)
 													 'unless))
 									 (push-statement (list (nth 0 stmt)
 																				 (nth 1 stmt))
-																	 (predecessors 1 statements))
+																	 (predecessors 1 *statements*))
 									 (push-statement (nth 2 stmt)
-																	 (predecessors 1 statements)))
+																	 (predecessors 1 *statements*)))
 									(t (push-statement stmt
-																		 (predecessors 1 statements))))))
+																		 (predecessors 1 *statements*))))))
 	program-code)
 
-
-(defun leader-predecessors ()
-	"loop through basic blocks w a5ali l predecessors bto3 l leader bta3 l block yeb2o vector of last-stmt-indexes of predecessors"
+(defun frst-stmt-predecessors ()
+	"loop through basic blocks w a5ali l predecessors bto3 l frst-stmt bta3 l block yeb2o vector of last-stmt-indexes of predecessors"
 	(loop
 		 :for basic-block
 		 :being :the :element :of *basic-blocks*
@@ -53,7 +52,7 @@
 							:being :the :element :of (predecessor-blocks basic-block)
 							:do (vector-push-extend (last-stmt (elt *basic-blocks* predecessor)) pre))
 					 (setf (predecessor-statements
-									(elt statements (leader basic-block))) pre))))
+									(elt *statements* (frst-stmt basic-block))) pre))))
 
 (defun predecessors (number v) ;;v : statemenets or *basic-blocks*
 	(if (zerop (length v))
@@ -69,22 +68,22 @@
 	(vector-push-extend (make-instance 'statement-block
 																		 :statement stmt
 																		 :predecessor-statements predecessors)
-											statements))
+											*statements*))
 
 (defun push-basic-block (predecessors)
 	(vector-push-extend (make-instance 'basic-block
-																		 :leader *leader*
+																		 :frst-stmt *frst-stmt*
 																		 :last-stmt *last-stmt*
 																		 :predecessor-blocks predecessors)
 											*basic-blocks*)
-	(setf *leader* (incf *last-stmt*)))
+	(setf *frst-stmt* (incf *last-stmt*)))
 
 (defun construct-basic-blocks ()
-	(let ((*leader* 0)
+	(let ((*frst-stmt* 0)
 				(*last-stmt* 0))
-		(declare (special *leader* *last-stmt*))
+		(declare (special *frst-stmt* *last-stmt*))
 		(loop
-			 :for stmt :in statements
+			 :for stmt :in *statements*
 			 :for x :from 0
 			 :do (cond ((eq (car stmt) 'if)
 									(push-basic-block (vector-union (predecessors 1 *basic-blocks*)
@@ -105,15 +104,13 @@
 
 (defun gen-and-initial-out ()
 	(loop
-		 :for stmt :being :the :element :of statements
+		 :for stmt :being :the :element :of *statements*
 		 :for x :from 0
-		 :do (let ((stmt-block (elt statements x))
-							 (stmt-out (make-array 1 :fill-pointer 0 :adjustable t)))
+		 :do (let ((stmt-out (make-array 1 :fill-pointer 0 :adjustable t)))
 					 (when (or (eq (car stmt) 'setq)(eq (car stmt) 'setf))
-						 (setf (gen stmt-block) (cons (nth 1 stmt) x))
-						 (vector-push-extend (gen stmt-block) stmt-out)
-						 (setf (out stmt-block) stmt-out)))))
-;;(find '(1 2) #(9 3 (1 2) 6 7 8) :test #'equal) => (1 2)
+						 (setf (gen stmt) (cons (nth 1 stmt) x))
+						 (vector-push-extend (gen stmt) stmt-out)
+						 (setf (out stmt) stmt-out)))))
 
 (defun vector-union (v w)
 	(remove-duplicates (merge 'vector v w #'equalp) :test #'equalp))
@@ -125,7 +122,7 @@
 		 :do (setq v (remove x v :test #'equalp)))
 	v)
 
-(defun reaching-definitions (statements)
+(defun reaching-definitions ()
 	(let ((change t)
 				(old-out))
 		(loop
@@ -133,7 +130,7 @@
 			 :do (setf change nil)
 			 (loop
 					:for stmt
-					:being :the :element :of statements
+					:being :the :element :of *statements*
 					:do (setf (in stmt) (in-set stmt))
 					(setf old-out (out stmt))
 					(setf (out stmt) (vector-union (gen stmt) (vector-difference (in stmt)(kill stmt))))
@@ -143,7 +140,7 @@
 (defun in-set (statement) ;;block .. magarabthash le7ad delwa2ti
 	(let ((in ))
 		(loop for predecessor-index being the element of (predecessor-statements statement)
-			 do (setf in (vector-union in (out (elt statements predecessor-index)))))
+			 do (setf in (vector-union in (out (elt *statements* predecessor-index)))))
 		in))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defparameter parsed-code '((setq x 12)
@@ -165,7 +162,6 @@
 
 (construct-statement-vector parsed-code)
 (construct-basic-blocks)
-(leader-predecessors basic-blocks)
-(construct-statement-vector parsed-code)
+(frst-stmt-predecessors basic-blocks)
 (gen-and-initial-out )
-;;(reaching-definitions statements)
+;;(reaching-definitions *statements*)
